395ae4cef5bf4627fb2091079629099af09a4f84
Marek Kuzora <marek.kuzora@gmail.com>
2011-04-19 23:06:48 +0200

    FIX: Problem with files starting from dot.

--- java/src/org/fierry/build/filters/ExtensionFileFilter.java
+++ java/src/org/fierry/build/filters/ExtensionFileFilter.java
@@ -11,7 +11,8 @@ public abstract class ExtensionFileFilter implements IFileFilter {
 		if(Files.isDirectory(project.getDirectory().resolve(path))) { return false; }
 		
 		String name = path.getFileName().toString();
-		String ext = name.substring(name.lastIndexOf('.'));
+		Integer idx = name.lastIndexOf('.');
+		String ext = idx < 0 ? name : name.substring(name.lastIndexOf('.'));
 
 		return ext.equals(expected);
 	}
